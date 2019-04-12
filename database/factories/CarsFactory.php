3abdafafcc078b785b3e50e2b4a12a472a2aac93
martellom24 <martellom24@gmail.com>
2019-04-12 12:53:57 -0400

    CarFactory Updated

--- database/factories/CarsFactory.php
+++ database/factories/CarsFactory.php
@@ -4,9 +4,14 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Cars::class, function (Faker $faker) {
 
+    // custom variables for make and year
+    $cars = array( "ford", "honda", "toyota" );
+    $randIndex = array_rand($cars);
+    $randYear = rand(1990, 2018);
+
     return [
-        'make' => $faker->sentence($nbWords = 6, $variableNbWords = true),
-        'model' => $faker->sentence($nbWords = 6, $variableNbWords = true),
-        'year' => $faker->sentence($nbWords = 6, $variableNbWords = true),
+        'make' => $cars[$randIndex],
+        'model' => $faker->sentence($nbWords = 1, $variableNbWords = true),
+        'year' => $randYear,
     ];
 });
